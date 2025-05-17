41d00ed7713d16d15f3cfd05de94b8327dc7e385
nazeroca <nazca294818@gmail.com>
2025-05-18 04:29:27 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+var CACHE_NAME = 'PWA';
+var urlsToCache = [
+    '/', '/index.html', '/manifest.json','/sw.js','./PWAimage/slime192.png', './PWAimage/slime512.png'
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
